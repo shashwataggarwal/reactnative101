fb195115319fbf1d8e3493ca82ab95dd8feea45f
Shashwat Aggarwal <shashwat4699@gmail.com>
2020-05-27 20:07:53 +0530

    Top navigation

--- App.js
+++ App.js
@@ -1,78 +1,128 @@
 import React, { useState } from 'react';
-import { Provider as PaperProvider, BottomNavigation, Button, Appbar, Text, Title, DefaultTheme } from 'react-native-paper';
-import { StyleSheet, SafeAreaView, StatusBar, Platform, View  } from 'react-native';
+import { Provider as PaperProvider, BottomNavigation, Button, Appbar, Text, Title, DefaultTheme, Card, Paragraph, Avatar, Chip, } from 'react-native-paper';
+import { StyleSheet, SafeAreaView, StatusBar, Platform, View, Image,  } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-let dict = {
-  index: 0,
-  routes: [
-    { key: 'music', title: 'Music', icon: 'music' },
-    { key: 'albums', title: 'Albums', icon: 'album' },
-    { key: 'recents', title: 'Recents', icon: 'history' },
-  ],
-}
+
+console.reportErrorsAsExceptions = false; // copy paste this line in your App.js 
+const Tab = createMaterialTopTabNavigator();
 
 
 export default function App() {
 
-  const [_i, setI] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'complaints', title: 'Complaints', icon: 'comment-alert-outline' },
+    { key: 'albums', title: 'Albums', icon: 'album' },
+    { key: 'recents', title: 'Recents', icon: 'history' },
+    { key: 'test', title: 'Testing', icon: 'battery' },
+  ]);
 
   StatusBar.setBarStyle('light-content');
-  StatusBar.setBackgroundColor('#c76512')
-  const _handleIndexChange = (index) => {
-    console.log(index);
-    _setState(prevState => {
-      console.log(`PREVIOUS INDEX = ${prevState.index}`)
-      prevState.index = index;
-      return ({...prevState});
-    });
-
-  };
+  StatusBar.setBackgroundColor('#c76512');
 
   const _renderScene = BottomNavigation.SceneMap({
-    music: MusicRoute,
+    complaints: ComplaintsRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+    test: TestRoute,
+  });
+
+  const [i, setI] = useState(0);
+  const [rout] = useState([
+    { key: 'albums', title: 'Albums', icon: 'album' },
+    { key: 'recents', title: 'Recents', icon: 'history' },
+  ]);
+  
+  const _renderI = BottomNavigation.SceneMap({
     albums: AlbumsRoute,
     recents: RecentsRoute,
   });
 
-
   return (
     <PaperProvider theme={theme}>
+
+      <NavigationContainer>
       <Appbar.Header>
+      <Avatar.Image size={37} source={require('./assets/shashwat.png')} style = {{paddingLeft:10}} onPress={()=> console.log('press')} />
       <Appbar.Content 
-        title = "LocalSewa"
+        title = "Complaints"
         style = {styles.center}
+        color = "#fff"
       />
+      <Appbar.Action icon="shopping" onPress={()=> console.log('kk')} />
     </Appbar.Header>
+    <Tab.Navigator tabBarOptions = {{
+        style: {
+          backgroundColor: 'green',
+          marginHorizontal: '10%',
+          width: '75%',
+          bord: 5
+        }
+    }}>
+      <Tab.Screen name="Home" component={AlbumsRoute} />
+      <Tab.Screen name="Settings" component={RecentsRoute} />
+    </Tab.Navigator>
     <BottomNavigation
-        navigationState={{
-          index: _i,
-          routes: [
-            { key: 'music', title: 'Music', icon: 'music' },
-            { key: 'albums', title: 'Albums', icon: 'album' },
-            { key: 'recents', title: 'Recents', icon: 'history' },
-          ]
-        }}
-        onIndexChange={(index) => setI(index)}
-        renderScene={_renderScene}
+        navigationState={{index, routes}}
+        onIndexChange = {setIndex}
+        renderScene = {_renderScene}
+        activeColor = "#fff"
+        shifting = {false}
       />
+      </NavigationContainer>
       </PaperProvider>
-    // <View style={styles.container, styles.droidSafeArea}>
-    //   <Text>{_state.index}</Text>
-    //   <Button onPress = {() => {
-    //     _handleIndexChange((_state.index+1)%3);
-    //     console.log("PRINTING STATE!");
-    //     console.log(_state);
-    //   }}>Click Me</Button>
-    // </View>
-    
+
   );
 }
 
-const MusicRoute = () => 
-<View style = {styles.container}>
-<Title>MUSIC</Title>
-</View>
+const ComplaintsRoute = () => {
+  const imageLeft = () => <Avatar.Image size={47} source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSsNUZfmqZRnO9cTsNxZ1dqlrI_zsBOVr30y6QgWp5pnF6Ikv5B&usqp=CAU' }} />
+  const labelRight = () => <Chip icon='check' style={{marginRight:4, backgroundColor:'#9df28a', }}>Resolved</Chip>
+  return (
+    <Card style={{margin:10}}>
+    <Card.Title title="Neil Armstrong" subtitle="7 hours ago" left={imageLeft} right={labelRight}/>
+    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    
+    <Card.Content>
+      <Title>Card title</Title>
+      <Paragraph>Card content</Paragraph>
+    </Card.Content>
+    <Card.Actions>
+      <Button>Cancel</Button>
+      <Button>Ok</Button>
+    </Card.Actions>
+  </Card>
+
+  )  
+}
 ;
+
+const TestRoute = () => {
+
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'albums', title: 'Albums', icon: 'album' },
+    { key: 'recents', title: 'Recents', icon: 'history' },
+  ]);
+
+  const _renderScene = BottomNavigation.SceneMap({
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+  });
+
+  return (
+   <BottomNavigation 
+   navigationState={{index, routes}}
+   onIndexChange = {setIndex}
+   renderScene = {_renderScene}
+   activeColor = "#fff"
+   theme = {secondTheme}
+   barStyle = {{position:'absolute', top:-10, height:20, marginHorizontal: '5%',}}
+   />
+  )
+}
 
 const AlbumsRoute = () => <View style = {styles.container}>
 <Title>ALBUMS</Title>
@@ -96,9 +146,8 @@ const styles = StyleSheet.create({
     paddingLeft:4
   },
   center: {
-    alignItems: 'center',
-    // justifyContent: 'center',
-  }
+    alignItems: 'center',  
+  },
 });
 
 const theme = {
@@ -109,5 +158,18 @@ const theme = {
     ...DefaultTheme.colors,
     primary: '#ff8217',
     accent: '#f1c40f',
+    placeholder: "#abcdef"
+  },
+};
+
+const secondTheme = {
+  ...DefaultTheme,
+  roundness: 2,
+  mode: 'adaptive',
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#0f8217',
+    accent: '#f1c40f',
+    placeholder: "#abcdef"
   },
 };
